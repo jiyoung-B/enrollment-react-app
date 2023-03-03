d90a594a810b302455373a7c6da4ae29374af61e
jiyoung-B <catgirl0313@naver.com>
2023-03-03 10:01:19 +0900

    코드 주석 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import EnrollmentForm from "./compoents/EnrollmentForm"
 
-
+// EnrollmentForm이라는 폼을 return하도록
+// 정의된 App 컴퍼넌트
 const App = () => {
 
     return(
@@ -11,4 +12,6 @@ const App = () => {
     );
 };
 
+// 컴퍼넌트나 모듈형태로 작성하는 경우
+// 기본적으로 내보내기할 함수명 지정
 export default App;
